8c8d820d2b9516be11181be9e5701126a51f8d38
Alexander Rubino <arubino@stevens.edu>
2021-03-05 11:35:30 -0500

    Resolved conflict with Zack's code (hopefully)

--- validation.py
+++ validation.py
@@ -50,6 +50,67 @@ def validate_birth_before_death(fams, indis):
   return return_data
 
 '''
+  Implements US06
+  Sprint 1
+  Zack Schieberl
+  A married couple cannot get divorced after one of them dies
+'''
+def validate_divorce_before_death(fams, indis):
+  ret_data = []
+  
+  for fid in fams:
+    if fams[fid]['DIV'] is not None:
+      divorce_date = utils.parse_date(fams[fid]['DIV'])
+      husband_id = fams[fid]['HUSB']
+      wife_id = fams[fid]['WIFE']
+
+      if indis[husband_id]['DEAT'] is not None:
+        death_date = utils.parse_date(indis[husband_id]['DEAT'])
+        if death_date < divorce_date:
+          ret_data.append((husband_id, f'Individual id={husband_id} has a divorce after his death'))
+      if indis[wife_id]['DEAT'] is not None:
+        death_date = utils.parse_date(indis[wife_id]['DEAT'])
+        if death_date < divorce_date:
+          ret_data.append((wife_id, f'Individual id={wife_id} has a divorce after her death'))
+  
+  return ret_data
+
+'''
+  Implements US07
+  Sprint 1
+  Zack Schieberl
+  An indiviual cannot be over 150 years old (alive or dead)
+'''
+def validate_reasonable_age(fams, indis):
+  ret_data = []
+  lifetime = 150
+  current_date = utils.current_date()
+  
+  for iid in indis:
+    death_date = None
+    if indis[iid]['DEAT'] is not None:
+      # If the input date is outside the scope of a datetime object,
+      # then return the error message
+      if not utils.parseable_date(indis[iid]['DEAT']):
+        ret_data.append((iid, f'Individual id={iid} is older than {lifetime} years'))
+        continue
+
+      death_date = utils.parse_date(indis[iid]['DEAT'])
+    else:
+      death_date = current_date
+
+    if indis[iid]['BIRT'] is not None:
+      if not utils.parseable_date(indis[iid]['BIRT']):
+        ret_data.append((iid, f'Individual id={iid} is older than {lifetime} years'))
+        continue
+
+      birth_date = utils.parse_date(indis[iid]['BIRT'])
+      if utils.get_age(birth_date, death_date) > lifetime:
+        ret_data.append((iid, f'Individual id={iid} is older than {lifetime} years'))
+  
+  return ret_data
+
+'''
   Implements US08
   
   Returns a list pairs:
